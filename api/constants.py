56a13c66aa462ffd4e3e1e6c2f97a91b01138a29
ngnquanq <tommyquanglowkey2011@gmail.com>
2025-01-08 22:52:17 +0700

    WIP
    Watchout the outlier, missing values and duplicates

--- api/constants.py
+++ api/constants.py
@@ -1,4 +1,4 @@
 COLS_TO_CAST =[...]
 CAT_COLS = [...]
 NUM_COLS = [...]
-SAMPLE_DATA = {...} # THINKING
+SAMPLE_DATA = {'id': 149990878, 'list_id': 109913621, 'list_time': 1694606265000, 'manufacture_date': 2011, 'brand': 'Ford', 'model': 'Fiesta', 'origin': 'Thái Lan', 'type': 'Hatchback', 'seats': 5.0, 'gearbox': 'AT', 'fuel': 'petrol', 'color': 'white', 'mileage_v2': 10000, 'price': 235000000.0, 'condition': 'used'} # A random sample
