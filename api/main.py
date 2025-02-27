ca7121425071d345ebecef221dc071817bd35860
ngnquanq <tommyquanglowkey2011@gmail.com>
2025-02-27 08:15:01 +0700

    Feat: Add logging

--- api/main.py
+++ api/main.py
@@ -1,3 +1,10 @@
+# For logging purposes
+import os 
+os.makedirs("/var/log/myapp", exist_ok=True)
+
+from loguru import logger
+logger.add("/var/log/myapp/app.log", rotation="10 MB", retention="10 days", level="INFO")
+
 from json import encoder
 from urllib import response
 from fastapi.testclient import TestClient
@@ -14,6 +21,9 @@ import sys
 import os 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from api import constants, preprocess
+
+# Log initial message 
+logger.info("Starting the ML application ...")
 
 # Setup some constants
 MODEL_PATH="models"
@@ -32,33 +42,32 @@ model_lgbm = lgb.Booster(model_file=f"{MODEL_PATH}/lgbm_model.joblib")
 
 @app.get("/")
 def read_root(request: Request):
+    logger.info("Serving index page")
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.post("/predict_lgbm")
 def predict_lgbm(car_data: CarPriceData):
-    # Convert input to dataframe
+    logger.info("Received LGBM prediction request")
     try:
         test_df = pd.DataFrame([car_data.dict()])
     except Exception as e:
+        logger.error("DataFrame conversion error: {}", e)
         raise HTTPException(status_code=400, detail=f"Error converting input to DataFrame: {str(e)}")
-
-    # Preprocess the data
+    
     try:
-        # Dropping unwanted columns
         test_df = preprocess.drop_unncessary_columns(df=test_df, cols_to_drop=constants.COLS_TO_DROP)
-        
-        # Convert categorical columns to category
         test_df = preprocess.cast_to_category(df=test_df, cols_to_cast=constants.CAT_COLS)
-        
     except Exception as e:
+        logger.error("Preprocessing error: {}", e)
         raise HTTPException(status_code=400, detail=f"Error during preprocessing: {str(e)}")
 
-    # Make prediction
     try:
         y_pred = model_lgbm.predict(test_df)
     except Exception as e:
+        logger.error("Prediction error: {}", e)
         raise HTTPException(status_code=500, detail=f"Error during prediction: {str(e)}")
     
+    logger.info("Prediction completed successfully")
     return float(y_pred[0])
 
 
